a5ceb4c452fbbd30baa5933ff7ad6c5342e5d021
Picvet <percyzulu2@outlook.com>
2023-05-17 14:21:00 +0200

    First commit for the application

---
+++ Notes/Notes/viewModel/TodoListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Notes.model;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Notes.viewModel
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem); 
+        public string NewTodoInputValue { get; set; }
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(object o)
+        {
+            TodoItem item = o as TodoItem;    
+            TodoItems.Remove(item);
+        }
+
+    }
+}
